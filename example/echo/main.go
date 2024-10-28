e2896b398cf0bd9b645bc4e6e2bef64134e685af
Mkamono <chengyaye31@gmail.com>
2024-10-28 16:34:22 +0000

    add echo example

---
+++ example/echo/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/Mkamono/slogcontext/slogcontext"
+	"github.com/Mkamono/slogcontext/slogcontext/adapter"
+	echoContextLogger "github.com/Mkamono/slogcontext/slogcontext/wrapper/echo"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	// Set up logger
+	baseHandler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		ReplaceAttr: adapter.NewReplacer(
+			adapter.SourceHierarchyRule(echoContextLogger.WrapHierarchy),
+		),
+	})
+	slogcontext.NewHandler(baseHandler)
+	slog.SetDefault(slog.New(baseHandler))
+
+	// Set up server
+	e.GET("/", func(c echo.Context) error {
+		// Log with context
+		echoContextLogger.WithValue(c, slogcontext.Attrs{"key": "value"})
+		echoContextLogger.InfoContext(c, "Hello, World!", slogcontext.Attrs{"key": "value"})
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
