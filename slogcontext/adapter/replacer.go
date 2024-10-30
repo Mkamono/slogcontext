651f1b1be692411f1408021259e006bc1193b258
Mkamono <chengyaye31@gmail.com>
2024-10-30 14:32:35 +0000

    change place

--- slogcontext/adapter/replacer.go
+++ slogcontext/adapter/replacer.go
@@ -3,11 +3,11 @@ package adapter
 import "log/slog"
 
 // implement Handler interface
+type replacer func(groups []string, a slog.Attr) slog.Attr
+
 var _ = slog.HandlerOptions{
 	ReplaceAttr: replacer(nil),
 }
-
-type replacer func(groups []string, a slog.Attr) slog.Attr
 
 type ReplaceRule func(a slog.Attr) slog.Attr
 
